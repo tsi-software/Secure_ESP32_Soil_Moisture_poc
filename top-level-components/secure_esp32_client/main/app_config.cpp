68372467a450b717746350128c284ac42721ebbb
Warren Taylor <git@taylorsoftware.ca>
2023-11-28 12:33:10 -0800

    secure_esp32_client - app_config - use unique_ptr for managing strings and blobs.

--- top-level-components/secure_esp32_client/main/app_config.cpp
+++ top-level-components/secure_esp32_client/main/app_config.cpp
@@ -23,22 +23,25 @@ AppConfig::AppConfig(const char *nvs_namespace) : nvs_namespace(nvs_namespace) {
     // nvs_handle is automatically closed on desctruction.
     nvs_handle = nvs::open_nvs_handle(nvs_namespace, NVS_READONLY, &err);
     if (err != ESP_OK) {
-        ESP_LOGW(LOG_TAG, "Error (%s) opening NVS handle for namespace '%s'!",
+        ESP_LOGW(LOG_TAG, "Warning (%s) opening NVS handle for namespace '%s'!",
                  esp_err_to_name(err), nvs_namespace);
     }
 }
 
 
-/**
- * The caller of this function is responsible for releasing the memory
- *  allocated and returned by this function.
- */
-char *AppConfig::get_str(const char *key)
+
+AppConfig::ConfigStr AppConfig::get_str(const char *key)
 {
     esp_err_t err;
+    ConfigStr null_result; // Default return value is an null unique_ptr.
+
+    if (!nvs_handle) {
+        ESP_LOGW(LOG_TAG, "Warning - get_str(%s) - NVS handle not opened for namespace '%s'!", key, nvs_namespace);
+        return null_result;
+    }
 
     // Read the size of memory space required.
-    size_t required_size = 0;  // value will default to 0, if not set yet in NVS
+    size_t required_size = 0;  // size value will default to 0 if not set yet in NVS.
     err = nvs_handle->get_item_size(nvs::ItemType::SZ, key, required_size);
     if (err != ESP_OK) {
         if (err == ESP_ERR_NVS_NOT_FOUND) {
@@ -48,22 +51,20 @@ char *AppConfig::get_str(const char *key)
             ESP_LOGE(LOG_TAG, "Error (%s) - KEY: '%s'!",
                      esp_err_to_name(err), key);
         }
-        return nullptr;
+        return null_result;
     }
 
-    // Read previously saved value if available.
-    char *buffer = nullptr;
+    // Read the previously saved value if available.
+    ConfigStr buffer;
     if (required_size > 0) {
-        // The caller of this function is responsible for releasing the memory allocated to 'buffer'.
         // Add 1 to guarantee a null terminator.
-        buffer = new char[required_size + sizeof(char)];
-        err = nvs_handle->get_string(key, buffer, required_size);
+        buffer = ConfigStr( new ConfigStr_T[required_size + 1] );
+        err = nvs_handle->get_string(key, buffer.get(), required_size);
         if (err == ESP_OK) {
             // DO NOT log 'buffer' here because it may contain sensitive information.
             buffer[required_size] = '\0'; // Guarantee a null terminator.
         } else {
-            delete[] buffer;
-            buffer = nullptr;
+            return null_result;
         }
     }
 
@@ -71,13 +72,16 @@ char *AppConfig::get_str(const char *key)
 }
 
 
-/**
- * The caller of this function is responsible for releasing the memory
- *  allocated and returned by this function.
- */
-void *AppConfig::get_blob(const char *key)
+
+AppConfig::ConfigBlob AppConfig::get_blob(const char *key)
 {
     esp_err_t err;
+    ConfigBlob null_result; // Default return value is an null unique_ptr.
+
+    if (!nvs_handle) {
+        ESP_LOGW(LOG_TAG, "Warning - get_blob(%s) - NVS handle not opened for namespace '%s'!", key, nvs_namespace);
+        return null_result;
+    }
 
     // Read the size of memory space required.
     size_t required_size = 0;  // value will default to 0, if not set yet in NVS
@@ -90,22 +94,20 @@ void *AppConfig::get_blob(const char *key)
             ESP_LOGE(LOG_TAG, "Error (%s) - KEY: '%s'!",
                      esp_err_to_name(err), key);
         }
-        return nullptr;
+        return null_result;
     }
 
-    // Read previously saved value if available.
-    char *buffer = nullptr;
+    // Read the previously saved value if available.
+    ConfigBlob buffer;
     if (required_size > 0) {
-        // The caller of this function is responsible for releasing the memory allocated to 'buffer'.
         // Add 1 to guarantee a null terminator.
-        buffer = new char[required_size + sizeof(char)];
-        err = nvs_handle->get_blob(key, buffer, required_size);
+        buffer = ConfigBlob( new ConfigBlob_T[required_size + 1] );
+        err = nvs_handle->get_blob(key, buffer.get(), required_size);
         if (err == ESP_OK) {
             // DO NOT log 'buffer' here because it may contain sensitive information.
-            buffer[required_size] = '\0'; // Guarantee a null terminator.
+            buffer[required_size] = ConfigBlob_T(0); // Guarantee a null terminator.
         } else {
-            delete[] buffer;
-            buffer = nullptr;
+            return null_result;
         }
     }
 
@@ -113,16 +115,20 @@ void *AppConfig::get_blob(const char *key)
 }
 
 
-// const char *AppConfig::get_app_uuid()
-// {
-//     if (!app_uuid) {
-//         app_uuid = get_str("app_uuid");
-//         if (app_uuid) {
-//             ESP_LOGI(LOG_TAG, "app_uuid: %s", app_uuid);
-//         }
-//     }
-//     return app_uuid;
-// }
+
+//------------------------------------------------------------------------------
+// WifiConfig
+//------------------------------------------------------------------------------
+const char *GlobalConfig::get_app_uuid()
+{
+    if (!app_uuid) {
+        app_uuid = get_str("app_uuid");
+        if (app_uuid) {
+            ESP_LOGI(LOG_TAG, "app_uuid: %s", app_uuid.get());
+        }
+    }
+    return app_uuid.get();
+}
 
 
 
@@ -134,10 +140,10 @@ const char *WifiConfig::get_wifi_ssid()
     if (!wifi_ssid) {
         wifi_ssid = get_str("wifi_ssid");
         if (wifi_ssid) {
-            ESP_LOGI(LOG_TAG, "wifi_ssid: '%s'", wifi_ssid);
+            ESP_LOGI(LOG_TAG, "wifi_ssid: '%s'", wifi_ssid.get());
         }
     }
-    return wifi_ssid;
+    return wifi_ssid.get();
 }
 
 
@@ -149,5 +155,5 @@ const char *WifiConfig::get_wifi_password()
             ESP_LOGI(LOG_TAG, "wifi_password: *redacted*");
         }
     }
-    return wifi_password;
+    return wifi_password.get();
 }
--- top-level-components/secure_esp32_client/main/app_config.hpp
+++ top-level-components/secure_esp32_client/main/app_config.hpp
@@ -11,19 +11,17 @@ app_config.hpp
 //------------------------------------------------------------------------------
 class AppConfig {
 public:
-    virtual ~AppConfig() {
-    }
+    virtual ~AppConfig() { }
 
 protected:
+    using ConfigStr_T  = char;
+    using ConfigBlob_T = std::byte;
+    using ConfigStr  = std::unique_ptr< ConfigStr_T[] >;
+    using ConfigBlob = std::unique_ptr< ConfigBlob_T[] >;
+
     AppConfig(const char *nvs_namespace);
-
-    // The caller of this function is responsible for releasing the memory
-    //  allocated and returned by this function.
-    char *get_str(const char *key);
-
-    // The caller of this function is responsible for releasing the memory
-    //  allocated and returned by this function.
-    void *get_blob(const char *key);
+    ConfigStr get_str(const char *key);
+    ConfigBlob get_blob(const char *key);
 
 private:
     std::unique_ptr<nvs::NVSHandle> nvs_handle;
@@ -32,27 +30,28 @@ private:
 
 
 
-// class GlobalConfig: public AppConfig {
-    //const char *get_app_uuid();
-//    char *app_uuid = nullptr;
-// }
-        //delete[] app_uuid;
+//------------------------------------------------------------------------------
+class GlobalConfig: public AppConfig {
+public:
+    GlobalConfig() : AppConfig("global") { }
+    const char *get_app_uuid();
+
+private:
+    ConfigStr app_uuid;
+};
+
 
 
 //------------------------------------------------------------------------------
 class WifiConfig: public AppConfig {
 public:
     WifiConfig() : AppConfig("wifi") { }
-    virtual ~WifiConfig() {
-        delete[] wifi_ssid;
-        delete[] wifi_password;
-    }
     const char *get_wifi_ssid();
     const char *get_wifi_password();
 
 private:
-    char *wifi_ssid = nullptr;
-    char *wifi_password = nullptr;
+    ConfigStr wifi_ssid;
+    ConfigStr wifi_password;
 };
 
 
