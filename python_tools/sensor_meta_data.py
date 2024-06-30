dbf2d050538e7d3d9c8f0fdb3aa80edeea72457d
Warren Taylor <wtaylor@taylorsoftware.ca>
2024-06-30 12:16:01 -0700

    soilmoisture_graph.py - sensor_meta_data class work in progress.

--- python_tools/sensor_meta_data.py
+++ python_tools/sensor_meta_data.py
@@ -35,7 +35,7 @@ class SensorMetaData:
     def find_sensor_uuid(self, sensor_uuid):
         """
         """
-        for sensor in self.raw_json_data['sensors']:
+        for sensor in self.raw_json_data['controllers']:
             if sensor['uuid'] == sensor_uuid:
                 return sensor
 
@@ -75,29 +75,29 @@ class SensorMetaData:
 
 
 
-    def iterate_sensors(self):
+    def iterate_controllers(self):
         """
+        Yield controller dictionaries.
         """
-        for sensor in self.raw_json_data['sensors']:
-            yield sensor
+        key = 'controllers'
+        for controller in self.raw_json_data[key]:
+            yield controller
 
 
 
-    def iterate_touch_sensors(self):
+    def iterate_touch_sensors(self, controller):
         """
+        Yield touch_sensor dictionaries with controller key,values merged in.
         """
         key = 'touch_sensors'
 
-        for sensor in self.iterate_sensors():
-            if key not in sensor:
-                # This chip is not configured with Touch Sensors.
-                continue
+        # In case the controller is not configured with Touch Sensors.
+        if key in controller:
+            controller_copy = controller.copy()
+            del controller_copy[key]
 
-            sensor_copy = sensor.copy()
-            del sensor_copy[key]
-
-            for touch_sensor in sensor[key]:
-                yield sensor_copy | touch_sensor
+            for touch_sensor in controller[key]:
+                yield controller_copy | touch_sensor
 
 
 
@@ -109,35 +109,67 @@ class SensorMetaData:
             ('#3 (1)', '#3 (2)', '#3 (3)', '#3 (4)'),
         ]
         """
-        group = []
         result = []
-        previous_name = ''
-        first_time = True
 
-        for touch_sensor in self.iterate_touch_sensors():
-            sensor_label = self.format_touch_sensor_label(touch_sensor['name'], touch_sensor['label'])
-            if previous_name != touch_sensor['name'] or first_time:
-                first_time = False
-                previous_name = touch_sensor['name']
-                group = [ sensor_label ]
-                result.append(group)
-            else:
+        for controller in self.iterate_controllers():
+            group = []
+
+            for touch_sensor in self.iterate_touch_sensors(controller):
+                sensor_label = self.format_touch_sensor_label(touch_sensor['name'], touch_sensor['label'])
                 group.append(sensor_label)
 
-        logger.info(pformat(result))
+            if bool(group):
+                result.append(group)
 
+        logger.info(pformat(result))
         return result
 
+        # group = []
+        # result = []
+        # previous_name = ''
+        # first_time = True
+        #
+        # for touch_sensor in self.iterate_touch_sensors():
+        #     sensor_label = self.format_touch_sensor_label(touch_sensor['name'], touch_sensor['label'])
+        #     if previous_name != touch_sensor['name'] or first_time:
+        #         first_time = False
+        #         previous_name = touch_sensor['name']
+        #         group = [ sensor_label ]
+        #         result.append(group)
+        #     else:
+        #         group.append(sensor_label)
+        #
+        # logger.info(pformat(result))
+        # return result
 
 
+
+    #TODO: rename to get_touch_sensor_colors(self)
     def get_line_colors(self):
         """
-        """
-        #JUST TESTING!
+        Return a dictionary of touch_sensor labels and their colors.
+        Example:
         return {
             '#2 (1)':'blue', '#2 (2)':'orange', '#2 (3)':'green', '#2 (4)':'red',
             '#3 (1)':'black', '#3 (2)':'cyan', '#3 (3)':'green', '#3 (4)':'red',
         }
+        """
+        default_color = 'grey'
+        result = {}
+
+        for controller in self.iterate_controllers():
+            for touch_sensor in self.iterate_touch_sensors(controller):
+                sensor_label = self.format_touch_sensor_label(touch_sensor['name'], touch_sensor['label'])
+
+                if 'color' in touch_sensor:
+                    color = touch_sensor['color']
+                else:
+                    color = default_color
+
+                result[sensor_label] = color
+
+        logger.info(pformat(result))
+        return result
 
 
 
